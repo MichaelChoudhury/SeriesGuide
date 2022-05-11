fe8573b7b2f14d65672993f63fb4baf529310677
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-05-11 16:51:12 +0200

    Use composable to build AboutActivity UI.

--- app/src/main/java/com/battlelancer/seriesguide/preferences/AboutActivity.kt
+++ app/src/main/java/com/battlelancer/seriesguide/preferences/AboutActivity.kt
@@ -2,27 +2,46 @@ package com.battlelancer.seriesguide.preferences
 
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import com.battlelancer.seriesguide.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.battlelancer.seriesguide.ui.SeriesGuidePreferences
+import com.google.android.material.composethemeadapter3.Mdc3Theme
 
 /**
- * Just hosts a [AboutPreferencesFragment].
+ * Displays details about the app version, links to credits and terms.
  */
-class AboutActivity : AppCompatActivity() {
+class AboutActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(SeriesGuidePreferences.THEME)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
-        setupActionBar()
+
+        setContent {
+            Mdc3Theme {
+                About()
+            }
+        }
     }
 
-    private fun setupActionBar() {
-        val toolbar = findViewById<Toolbar>(R.id.sgToolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    @Composable
+    fun About() {
+        Text(text = "About SeriesGuide")
     }
+
+    @Preview
+    @Composable
+    fun AboutPreview() {
+        About()
+    }
+
+//    private fun setupActionBar() {
+//        val toolbar = findViewById<Toolbar>(R.id.sgToolbar)
+//        setSupportActionBar(toolbar)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
